eeb826d393aa9748d70df9df610e0f20f478d45d
Chris Larson <c.lars701@gmail.com>
2019-01-19 15:40:24 -0800

    add get image route

--- look/images.py
+++ look/images.py
@@ -1,12 +1,13 @@
 import io
 import os
+import re
 import uuid
 import mimetypes
 
 import falcon
 import msgpack
 
-class Resource(object):
+class Collection(object):
 
     def __init__(self, image_store):
         self._image_store = image_store
@@ -27,14 +28,26 @@ class Resource(object):
     def on_post(self, req, resp):
         name = self._image_store.save(req.stream, req.content_type)
         resp.status = falcon.HTTP_201
-        resp.location = '/images/' + name 
+        resp.location = '/images/' + name
+
+
+class Item(object):
+
+    def __init__(self, image_store):
+        self._image_store = image_store
+
+    def on_get(self, req, resp, name):
+        resp.content_type = mimetypes.guess_type(name)[0]
+        resp.stream, resp.stream_len = self._image_store.open(name)
+
 
 class ImageStore(object):
 
     _CHUNK_SIZE_BYTES = 4096
+    _IMAGE_NAME_PATTERN = re.compile(
+        r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.[a-z]{2,4}$'
+    )
 
-    # Note the use of dependency injection for standard libary
-    # methods. We'll use these later to avoid monkey-patching.
     def __init__(self, storage_path, uuidgen=uuid.uuid4, fopen=io.open):
         self._storage_path = storage_path
         self._uuidgen = uuidgen
@@ -53,4 +66,15 @@ class ImageStore(object):
 
                 image_file.write(chunk)
 
-            return name
+        return name
+
+    def open(self, name):
+        # Always validate untrusted input!
+        if not self._IMAGE_NAME_PATTERN.match(name):
+            raise IOError('File not found')
+
+        image_path = os.path.join(self._storage_path, name)
+        stream = self._fopen(image_path, 'rb')
+        stream_len = os.path.getsize(image_path)
+
+        return stream, stream_len
